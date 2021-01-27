3991c36d63bd005b93bc94fa86bfc2c7ace939a7
WatchfulSleeper <64140316+WatchfulSleeper@users.noreply.github.com>
2021-01-27 10:58:56 +0100

    Logging for scripts
    English:
    This piece of code allows logging to a script file. You can define a location in which the script will work and save logs.
    
    Requirements: 
    pathlib
    datetime
    Česky:
    Tento kus kódu umožňuje logování scriptu do souboru. Lze nadefinovat místo, v jaké složce bude script pracovat a ukládat logy.
    
    Požadavky:
    pathlib
    datetime

---
+++ Logging.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import datetime
+
+workdir = Path.cwd()
+
+def logging(severity, Messange):
+    directoryExist = Path.exists(workdir / 'Logs')
+    severityString = 'UNKNOWN'
+    currentDate = datetime.datetime.today().strftime ('%Y-%m-%d')
+    currentTime = datetime.datetime.today().strftime('%Y-%m-%d %H:%M:%S,%f')
+    if not directoryExist:
+        Path.mkdir('Logs')
+    logFileName = currentDate + '-daily-log.log'
+    fileExist = Path.exists(workdir / 'Logs' / logFileName)
+    if severity == 1:
+        severityString = 'DEBUG'
+    elif severity == 2:
+        severityString = 'INFORMATION'
+    elif severity == 3:
+        severityString = 'ERROR'
+    else:
+        severityString = 'UNKNOWN'
+    if fileExist:
+        with open(workdir / 'Logs' / logFileName, 'a+', encoding="utf8") as log_file:
+            log_file.write(currentTime + ':' + severityString + ': ' + Messange + '\n')
+    else:
+        with open(workdir / 'Logs' / logFileName, 'w', encoding="utf8") as log_file:
+            log_file.write(currentTime + ':' + severityString + ': ' + Messange + '\n')
+logging(2, 'Script has been start!')
+
+logging(2, 'Script has been stop!')
